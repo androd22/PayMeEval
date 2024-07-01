9c6fe297df5e7542b8624893b12ae06e6036e6b2
androd22 <andy_14@hotmail.fr>
2024-07-01 12:16:25 +0200

    role + nbpillage +  totalsommespillees

---
+++ src/main/java/com/example/PayMe/controller/UserController.java
@@ -0,0 +1,78 @@
+package com.example.PayMe.controller;
+
+import com.example.PayMe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.PayMe.model.User;
+import com.example.PayMe.service.UserService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@GetMapping("/user")
+	public String getUserProfil(Model model) {
+		User user = userService.getConnectedUser();
+		model.addAttribute("listContact", user.getListContacts());
+		return "userProfil";
+	}
+
+
+	@PostMapping("/addContact")
+	public String addContact(@RequestParam(value = "name") String name, Model model) {
+		User user = userService.getConnectedUser();
+		User userToAdd = userService.getUserByName(name);
+		if (userToAdd != null) {
+			if (!user.getListContacts().contains(userToAdd) && user != userToAdd) {
+				user.getListContacts().add(userToAdd);
+				userService.save(user);
+			}
+			;
+		}
+		model.addAttribute("listContact", user.getListContacts());
+
+		return "userProfil";
+	}
+
+
+	@GetMapping("/admin")
+	public String getUsersAdmin(Model model, RedirectAttributes ra) {
+		model.addAttribute("users", userService.getAllUser());
+		return "adminUsers";
+
+
+	}
+
+	@GetMapping("/darkadmin")
+	public String getUsersDarkAdmin(Model model, RedirectAttributes ra) {
+		model.addAttribute("users", userService.getAllUser());
+		return "darkadminusers";
+	}
+
+//	@PostMapping("users/save")
+//	public String saveUser(User user, RedirectAttributes ra) {
+//		userRepository.save(user);
+//		return "redirect:/darkadminusers";
+//	}
+
+	@PostMapping("/users/pillage")
+	public String pillageUser(@RequestParam("id") Long id, @RequestParam("totalSommePillees") int totalSommePillees) {
+		User user = userRepository.findById(id).orElse(null);
+		if (user != null) {
+			user.setTotalSommePillees(totalSommePillees + user.getTotalSommePillees());
+			user.setNbPillage(user.getNbPillage() + 1);
+			userRepository.save(user);
+		}
+		return "redirect:/darkadmin";
+	}
+}
+
---
+++ src/main/java/com/example/PayMe/model/User.java
@@ -0,0 +1,151 @@
+package com.example.PayMe.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	
+	private float balance;
+	
+	private String iban;
+	
+	private String email;
+	
+	private String pwd;
+
+	private int nbPillage;
+
+	private int totalSommePillees;
+
+	public int getNbPillage() {
+		return nbPillage;
+	}
+
+	public void setNbPillage(int nbPillage) {
+		this.nbPillage = nbPillage;
+	}
+
+	public int getTotalSommePillees() {
+		return totalSommePillees;
+	}
+
+	public void setTotalSommePillees(int totalSommePillees) {
+		this.totalSommePillees = totalSommePillees;
+	}
+
+	@ManyToMany
+	private List<User> listContacts;
+	
+	@OneToMany(mappedBy ="sender")
+	private List<Transaction> listeTransaction;
+	
+	@OneToMany(mappedBy ="user")
+	private List<Virement> listeVirement;
+	
+	public void changeAmount(float toAdd) {
+		this.balance += toAdd;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getBalance() {
+		return balance;
+	}
+
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+
+	public String getIban() {
+		return iban;
+	}
+
+	public void setIban(String iban) {
+		this.iban = iban;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	public List<User> getListContacts() {
+		return listContacts;
+	}
+
+	public void setListContacts(List<User> listContacts) {
+		this.listContacts = listContacts;
+	}
+
+	public List<Transaction> getListeTransaction() {
+		return listeTransaction;
+	}
+
+	public void setListeTransaction(List<Transaction> listeTransaction) {
+		this.listeTransaction = listeTransaction;
+	}
+
+	public List<Virement> getListeVirement() {
+		return listeVirement;
+	}
+
+	public void setListeVirement(List<Virement> listeVirement) {
+		this.listeVirement = listeVirement;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				", balance=" + balance +
+				", iban='" + iban + '\'' +
+				", email='" + email + '\'' +
+				", pwd='" + pwd + '\'' +
+				", nbPillage=" + nbPillage +
+				", totalSommePillees=" + totalSommePillees +
+				", listContacts=" + listContacts +
+				", listeTransaction=" + listeTransaction +
+				", listeVirement=" + listeVirement +
+				'}';
+	}
+}
